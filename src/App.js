9a463e4007703e1c9e08042b7f043bda4677a351
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-28 14:31:24 +0100

    updating props for card section

--- src/App.js
+++ src/App.js
@@ -5,12 +5,31 @@ import Nav from './js-components/nav';
 import Hero from './js-components/hero';
 import Card from './js-components/card';
 
+// importing props components 
+import picture from '../src/assets/card-assets/image-12.png';
+
 export default function App () {
   return (
     <div>
       <Nav />
       <Hero />
-      <Card />
+      <Card 
+        img = {picture}
+        status = "sold out"
+        ratingnum = "5.0"
+        ratingcount = {6}
+        location = "USA"
+        description = "Life lessons with Katie Zaferes"
+        price = {136}
+      />
     </div>
   )
 }
+
+        // img = ""
+        // status = ""
+        // rating-num = ""
+        // rating-count = ""
+        // location = ""
+        // description = ""
+        // price = ""
--- src/js-components/card.js
+++ src/js-components/card.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import img from '../../src/assets/card-assets/image-12.png';
 import {ReactComponent as Star} from '../assets/card-assets/Star-1.svg';
 
-export default function Card () {
+export default function Card (props) {
     return (
         <div className='card-container'>
             <div className='card'>
-            <img src={img} className='profile-img' alt='profile image' />
-            <div className='status'><p className='status-text'>sold out</p></div>
+                <img src={props.img} className='profile-img' alt='profile image' />
+                <div className='status'><p className='status-text'>{props.status}</p></div>
 
-            <div className='rating-and-location'>
-                <Star />
-                <p className='rating-num'>5.0</p>
-                <p className='rating-count'>(6)</p>
-                <p className='location'>&#8226; USA</p>
-            </div>
+                <div className='rating-and-location'>
+                    <Star />
+                    <p className='rating-num'>{props.ratingnum}</p>
+                    <p className='rating-count'>({props.ratingcount})</p>
+                    <p className='location'>&#8226; {props.location}</p>
+                </div>
 
-            <div className='card-text'>
-                <p className='description'>Life lessons with Katie Zaferes</p>
-                <p className='pricing'>From <span className='price-per-person'>$136</span> <span className='span-2'>/ person</span></p>
-            </div>
+                <div className='card-text'>
+                    <p className='description'>{props.description}</p>
+                    <p className='pricing'>From $<span className='price-per-person'>{props.price}</span> <span className='span-2'>/ person</span></p>
+                </div>
 
             </div>
         </div>
