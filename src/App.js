1825a3932e99216d1560f8f45f50223b622cf72f
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-29 17:50:04 +0100

    adding custom data

--- src/App.js
+++ src/App.js
@@ -4,32 +4,35 @@ import './App.css';
 import Nav from './js-components/nav';
 import Hero from './js-components/hero';
 import Card from './js-components/card';
+import users from './js-components/data';
+
+import { Axios } from 'axios';
 
 // importing props components 
 import picture from '../src/assets/card-assets/image-12.png';
+import { upload } from '@testing-library/user-event/dist/upload';
 
 export default function App () {
+const userDetails = users.map(user => {
+  return <Card 
+          img = {user.coverImg}
+          status = {user.openSpots}
+          ratingNum = {user.stats.rating}
+          ratingCount = {user.stats.reviewCount}
+          location = {user.location}
+          title = {user.title}
+          price = {user.price}
+       />
+
+})
+
   return (
     <div>
       <Nav />
       <Hero />
-      <Card 
-        img = {picture}
-        status = "sold out"
-        ratingnum = "5.0"
-        ratingcount = {6}
-        location = "USA"
-        description = "Life lessons with Katie Zaferes"
-        price = {136}
-      />
+      {userDetails}
     </div>
   )
 }
 
-        // img = ""
-        // status = ""
-        // rating-num = ""
-        // rating-count = ""
-        // location = ""
-        // description = ""
-        // price = ""
+    
--- src/js-components/card.js
+++ src/js-components/card.js
@@ -5,18 +5,18 @@ export default function Card (props) {
     return (
         <div className='card-container'>
             <div className='card'>
-                <img src={props.img} className='profile-img' alt='profile image' />
-                <div className='status'><p className='status-text'>{props.status}</p></div>
+                <img src  = {props.img} className='profile-img' alt='profile image' />
+                {props.status === 0 && <div className='status'><p className='status-text'>SOLD OUT</p></div>}
 
                 <div className='rating-and-location'>
                     <Star />
-                    <p className='rating-num'>{props.ratingnum}</p>
-                    <p className='rating-count'>({props.ratingcount})</p>
+                    <p className='rating-num'>{props.ratingNum}</p>
+                    <p className='rating-count'>({props.ratingCount})</p>
                     <p className='location'>&#8226; {props.location}</p>
                 </div>
 
                 <div className='card-text'>
-                    <p className='description'>{props.description}</p>
+                    <p className='description'>{props.title}</p>
                     <p className='pricing'>From $<span className='price-per-person'>{props.price}</span> <span className='span-2'>/ person</span></p>
                 </div>
 
