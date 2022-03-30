9f033d2f36f768f1bce51a6603d3b06fa69b0cab
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-30 07:20:59 +0100

    updating solution

--- src/App.js
+++ src/App.js
@@ -9,20 +9,14 @@ import users from './js-components/data';
 import { Axios } from 'axios';
 
 // importing props components 
-import picture from '../src/assets/card-assets/image-12.png';
-import { upload } from '@testing-library/user-event/dist/upload';
 
 export default function App () {
 const userDetails = users.map(user => {
-  return <Card 
-          img = {user.coverImg}
-          status = {user.openSpots}
-          ratingNum = {user.stats.rating}
-          ratingCount = {user.stats.reviewCount}
-          location = {user.location}
-          title = {user.title}
-          price = {user.price}
+  return (<Card 
+          key = {user.id}
+          user = {user}
        />
+  )
 
 })
 
--- src/js-components/card.js
+++ src/js-components/card.js
@@ -2,22 +2,31 @@ import React from 'react';
 import {ReactComponent as Star} from '../assets/card-assets/Star-1.svg';
 
 export default function Card (props) {
+    let badgeText;
+    if (props.user.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.user.location === 'Online') {
+        badgeText = "ONLINE";
+    }
+
+
+
     return (
         <div className='card-container'>
             <div className='card'>
-                <img src  = {props.img} className='profile-img' alt='profile image' />
-                {props.status === 0 && <div className='status'><p className='status-text'>SOLD OUT</p></div>}
+                <img src  = {props.user.coverImg} className='profile-img' alt='profile image' />
+                {badgeText && <div className='status'><p className='status-text'>{badgeText}</p></div>}
 
                 <div className='rating-and-location'>
                     <Star />
-                    <p className='rating-num'>{props.ratingNum}</p>
-                    <p className='rating-count'>({props.ratingCount})</p>
-                    <p className='location'>&#8226; {props.location}</p>
+                    <p className='rating-num'>{props.user.stats.rating}</p>
+                    <p className='rating-count'>({props.user.stats.reviewCount})</p>
+                    <p className='location'>&#8226; {props.user.location}</p>
                 </div>
 
                 <div className='card-text'>
-                    <p className='description'>{props.title}</p>
-                    <p className='pricing'>From $<span className='price-per-person'>{props.price}</span> <span className='span-2'>/ person</span></p>
+                    <p className='description'>{props.user.title}</p>
+                    <p className='pricing'>From $<span className='price-per-person'>{props.user.price}</span> <span className='span-2'>/ person</span></p>
                 </div>
 
             </div>
